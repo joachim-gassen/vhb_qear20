4c6c5fdeba0fc2e07b17b68c056a649626f12415
jw29 <60785422+jw29@users.noreply.github.com>
2020-08-26 09:56:08 +0200

    reading in data?

--- code/codebook.R
+++ code/codebook.R
@@ -1,2 +1,5 @@
 Codebook 
 
+directory <- "C:/Users/wagne/Documents/vhb_qear20/raw_data"
+insolv <- file.path(directory, "insolvency_filings_de_julaug2020_incomplete.csv")
+dat1 <- read.csv(file = insolv, header = TRUE)
